94c4b3336fb840991d0ee176479eeee90b0818a9
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-06-12 00:22:23 +0100

    Create basic post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    # Unique ID for each post
+    id = models.AutoField(primary_key=True)
+
+    # Array of tags for each post
+    # from django.contrib.postgres.fields import ArrayField
+    # tags = ArrayField(models.CharField(max_length=100), blank=True, null=True)
+    # For now we will just store them as a string separated by spaces
+    tags = models.CharField(max_length=10000, blank=True, null=True)
+
+    # SFW Rating (either safe, questionable, or explicit) (not null, default safe)
+    rating = models.CharField(max_length=10, default='safe')
+
+    # Score (i.e. number of upvotes minus number of downvotes), not null, default 0
+    score = models.IntegerField(default=0)
+
+    # Post md5 checksum
+    md5 = models.CharField(max_length=32)
+
+    # Sample flag (i.e. true when there is a sample for the post - happens when the post is a large image)
+    sample = models.BooleanField(default=False)
+
+    # Post folder (which folder in the storage directory the post is stored in) (this is indicated as an integer)
+    folder = models.IntegerField()
+
+    # Owner as a user id foreign key, but for now we will just store the owner as a string
+    # TODO add foreign key to user table
+    owner = models.CharField(max_length=100, blank=True, null=True)
+
+    # Makes if the post is a video type or not
+    is_video = models.BooleanField(default=False)
+
+    # Post width
+    width = models.IntegerField()
+
+    # Post height
+    height = models.IntegerField()
+
+    # Post timestamp (i.e. when the post was uploaded) (default current timestamp)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # Source as a URL to the original post
+    source = models.CharField(max_length=1000, blank=True, null=True)
