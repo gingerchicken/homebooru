2cb914e199e2265d3fef9bdb6247dc19eaceca11
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-06-12 20:18:16 +0100

    Add unit test env
    #1

--- start.sh
+++ start.sh
@@ -6,5 +6,14 @@ python manage.py makemigrations
 # Migrate the database
 python manage.py migrate --run-syncdb
 
+# Check for the unit test enviroment variable
+# If it is set and equal to "True" then run the unit tests
+if [ "$UNIT_TEST" = "True" ]; then
+    python manage.py test
+
+    # Exit with the unit test exit code
+    exit $?
+fi
+
 # Start the application
 python manage.py runserver 0.0.0.0:8000
