7819d9d5a07d5d8a8ed2253dd09edd627dcba9a1
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-06-12 21:15:00 +0100

    Add basic index unit tests

--- booru/tests.py
+++ booru/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Test the index page
+class IndexTest(TestCase):
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_imagecounter(self):
+        response = self.client.get('/')
+        self.assertContains(response, '<div id="imagecounter">')
+        self.assertContains(response, '<img src="/static/layout/')
+    
