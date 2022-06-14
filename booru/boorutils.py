b705402937f6a23334f0d4568b97e7c67acf56d4
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-06-14 20:28:35 +0100

    Add file checksum function

--- booru/boorutils.py
+++ booru/boorutils.py
@@ -6,6 +6,23 @@ def hash_str(s):
     """Hashes a string using the md5 algorithm"""
     return hashlib.md5(s.encode('utf-8')).hexdigest()
 
+def get_file_checksum(path : str) -> str:
+    """Gets the checksum of a file"""
+
+    # Make sure that the file exists
+    file_path = pathlib.Path(path)
+
+    if not file_path.exists():
+        raise Exception("File does not exist")
+
+    # Make the file path absolute
+    file_path = file_path.resolve()
+
+    # Get the file md5 checksum
+    md5 = hashlib.md5(file_path.read_bytes()).hexdigest()
+
+    return md5
+
 def get_content_dimensions(path : str) -> (int, int):
     """Gets the dimensions of an image or video"""
 
--- booru/tests/boorutils.py
+++ booru/tests/boorutils.py
@@ -12,3 +12,8 @@ class GetContentDimensionsTest(TestCase):
 
     def test_get_content_dimensions_video(self):
         self.assertEqual(get_content_dimensions("assets/TEST_DATA/content/ana_cat.mp4"), (928, 1904))
+
+class GetFileChecksumTest(TestCase):
+    def test_get_file_checksum(self):
+        self.assertEqual(get_file_checksum("assets/TEST_DATA/content/felix.jpg"),   "2dcd09f6c874b36355336112d17434e1")
+        self.assertEqual(get_file_checksum("assets/TEST_DATA/content/ana_cat.mp4"), "11e0a9c6b20d54593b8fc8f134a25256")
