ee65a420f116b7a3110e56be9254ea4434a8d7b4
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-06-15 00:29:00 +0100

    Add generate thumbnail function

--- booru/boorutils.py
+++ booru/boorutils.py
@@ -2,6 +2,11 @@ import hashlib
 import pathlib
 import ffmpegio
 
+import homebooru.settings
+
+# show log constant
+__SHOW_LOG = True if homebooru.settings.DEBUG else None
+
 def hash_str(s):
     """Hashes a string using the md5 algorithm"""
     return hashlib.md5(s.encode('utf-8')).hexdigest()
@@ -36,10 +41,33 @@ def get_content_dimensions(path : str) -> (int, int):
     file_path = file_path.resolve()
 
     # Use ffmpeg to get the dimensions
-    pixels = ffmpegio.image.read(str(file_path))
+    pixels = ffmpegio.image.read(str(file_path), show_log=__SHOW_LOG)
 
     # get the dimensions
     (height, width) = pixels.shape[:2]
 
     # Make it the right way round!
     return (width, height)
+
+def generate_thumbnail(path : str, save_path : str) -> str:
+    """Generates a thumbnail for a post"""
+
+    # Make sure that the file exists
+    file_path = pathlib.Path(path)
+    file_save_path = pathlib.Path(save_path)
+
+    if not file_path.exists():
+        raise Exception("File does not exist")
+    
+    # Make both paths absolute
+    file_path = file_path.resolve()
+    file_save_path = file_save_path.resolve()
+
+    # Use ffmpeg to generate the thumbnail (i.e. create a new image with the resolution of ?x150)
+    ffmpegio.transcode(file_path, file_save_path, overwrite=True, show_log=__SHOW_LOG, **{"vf": "scale=-1:150", "vframes": "1"})
+
+    # Check that the file was created
+    if not file_save_path.exists():
+        raise Exception("File was not created")
+    
+    return True
--- booru/tests/boorutils.py
+++ booru/tests/boorutils.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+import pathlib
+
 from ..boorutils import *
 
 class HashStrTest(TestCase):
@@ -17,3 +19,56 @@ class GetFileChecksumTest(TestCase):
     def test_get_file_checksum(self):
         self.assertEqual(get_file_checksum("assets/TEST_DATA/content/felix.jpg"),   "2dcd09f6c874b36355336112d17434e1")
         self.assertEqual(get_file_checksum("assets/TEST_DATA/content/ana_cat.mp4"), "11e0a9c6b20d54593b8fc8f134a25256")
+
+class GenerateThumbnailTest(TestCase):
+    original_image = "assets/TEST_DATA/content/felix.jpg"
+    output_image   = "/tmp/thumbnail_felix.png"
+
+    def setUp(self):
+        if pathlib.Path(self.output_image).exists():
+            # Delete it
+            pathlib.Path(self.output_image).unlink()
+
+    def test_generates_a_file(self):
+        self.assertTrue(generate_thumbnail(self.original_image, self.output_image))
+
+        # Check that the file exists
+        self.assertTrue(pathlib.Path(self.output_image).exists())
+    
+    def test_generates_a_file_with_the_correct_dimensions(self):
+        self.assertTrue(generate_thumbnail(self.original_image, self.output_image))
+
+        # Check that the file exists
+        self.assertTrue(pathlib.Path(self.output_image).exists())
+
+        # Check that the file has the correct dimensions
+        original_dims = get_content_dimensions(self.original_image)
+        output_dims = get_content_dimensions(self.output_image)
+        
+        r = float(150) / float(original_dims[1])
+        expected_dims = (int(r * original_dims[0]), 150)
+
+        self.assertEqual(output_dims, expected_dims)
+    
+    def test_generates_a_file_for_a_video(self):
+        self.assertTrue(generate_thumbnail("assets/TEST_DATA/content/ana_cat.mp4", self.output_image))
+
+        # Check that the file exists
+        self.assertTrue(pathlib.Path(self.output_image).exists())
+    
+    def test_generates_a_file_for_a_video_with_the_correct_dimensions(self):
+        self.assertTrue(generate_thumbnail("assets/TEST_DATA/content/ana_cat.mp4", self.output_image))
+
+        # Check that the file exists
+        self.assertTrue(pathlib.Path(self.output_image).exists())
+
+        # Check that the file has the correct dimensions
+        original_dims = get_content_dimensions("assets/TEST_DATA/content/ana_cat.mp4")
+        output_dims = get_content_dimensions(self.output_image)
+
+        r = float(150) / float(original_dims[1])
+        expected_dims = (int(r * original_dims[0]), 150)
+
+        self.assertEqual(output_dims, expected_dims)
+    
+    # TODO test for invalid/corrupt files
