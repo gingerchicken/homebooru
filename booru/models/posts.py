39c6a705d198cf62a398e9f5492d0f4564c59a95
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-06-23 19:19:45 +0100

    Create criteria file

--- booru/models/posts.py
+++ booru/models/posts.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from .tags import Tag, TagType
+from .posts_search_criteria import *
 
 import homebooru.settings as settings
 import booru.boorutils as boorutils
@@ -28,7 +29,6 @@ class Rating(models.Model):
     
     def __str__(self):
         return self.name
-
 
 class Post(models.Model):
     """A post is a picture or video that has been uploaded to the site."""
@@ -444,71 +444,3 @@ class Post(models.Model):
             'older': older,
             'newer': newer
         }
-
-# Search criteria for the post search
-
-class SearchCriteria:
-    """Interface for creating search criteria"""
-
-    def __init__(self) -> None:
-        pass
-
-    def search(self, s) -> models.QuerySet:
-        return s
-
-class SearchCriteriaTag(SearchCriteria):
-    """Used to search for posts that contain a certain tag"""
-
-    def __init__(self, tag: Tag) -> None:
-        self.tag = tag
-
-    def search(self, s) -> models.QuerySet:
-        return s.filter(tags=self.tag)
-
-class SearchCriteriaExcludeTag(SearchCriteria):
-    """Used to exclude tags from a search"""
-
-    def __init__(self, tag: Tag) -> None:
-        self.tag = tag
-
-    def search(self, s) -> models.QuerySet:
-        return s.exclude(tags=self.tag)
-
-class SearchCriteriaWildCardTags(SearchCriteria):
-    """Used to search for posts that contain a certain tag"""
-
-    def __init__(self, tags: [Tag]) -> None:
-        self.tags = tags
-
-    def search(self, s) -> models.QuerySet:
-        # It must include at least one of the tags, without duplicates
-        return s.filter(tags__in=self.tags).distinct('id')
-
-class SearchCriteriaExcludeWildCardTags(SearchCriteria):
-    """Used to exclude tags from a search"""
-
-    def __init__(self, tags: [Tag]) -> None:
-        self.tags = tags
-
-    def search(self, s) -> models.QuerySet:
-        return s.exclude(tags__in=self.tags).distinct('id')
-
-class SearchCriteriaParameter(SearchCriteria):
-    """Used to search for posts that have a certain parameter"""
-
-    def __init__(self, parameter: str, value: str) -> None:
-        self.parameter = parameter
-        self.value = value
-
-    def search(self, s) -> models.QuerySet:
-        return s.filter(**{self.parameter: self.value})
-
-class SearchCriteriaExcludeParameter(SearchCriteria):
-    """Used to exclude posts that have a certain parameter"""
-
-    def __init__(self, parameter: str, value: str) -> None:
-        self.parameter = parameter
-        self.value = value
-
-    def search(self, s) -> models.QuerySet:
-        return s.exclude(**{self.parameter: self.value})
