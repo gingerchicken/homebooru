341cbc747235cadb280c83e5730832e553d0dfef
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-06-26 17:48:38 +0100

    Add tag search web backend

--- booru/views/tags.py
+++ booru/views/tags.py
@@ -9,10 +9,31 @@ def tags(request):
     # Get the search phrase url parameter
     search_phrase = request.GET.get('tag', '').strip()
 
-    # TODO implement search functionality
+    # Get the page number url parameter
+    page_number = request.GET.get('pid', '1')
 
-    # For now we will just get all of the tags
-    tags = Tag.objects.all()
+    # Get the order_by url parameter
+    order_by = request.GET.get('order_by', 'total_posts')
+
+    # Get the order direction url parameter
+    order_direction = request.GET.get('order_direction', 'descending')
+
+    # Make sure that the page number is an integer
+    try:
+        page_number = int(page_number)
+    except ValueError:
+        page_number = 1
+    
+    # Get the tags
+    tags = Tag.objects.none()
+
+    # Handle any invalid inputs
+    try:
+        tags = Tag.search(search_phrase, sort_param=order_by, order=order_direction)
+    except ValueError:
+        # Send a 400 error as this will be a bad request
+        # TODO create a custom error page for 400 errors
+        return HttpResponse(status=400)
 
     # Render the tags.html template with the tags
     return render(request, 'booru/tags/tags.html', {
