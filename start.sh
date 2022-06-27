a43ce38796ac95c823dc5f8ba697ecd300c684aa
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-06-27 23:29:28 +0100

    Omit migrations from coverage

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ if [ "$UNIT_TEST" = "True" ]; then
     fi
 
     # Run the unit tests
-    coverage run --omit=*/tests/*.py --source='./booru' manage.py test
+    coverage run --omit=*/tests/*.py,*/migrations/*.py --source='./booru' manage.py test
 
     # Save the exit code of the unit tests
     UNIT_TEST_EXIT_CODE=$?
