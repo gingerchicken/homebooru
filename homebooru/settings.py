a1049b4b9806633f5faa2d43be72de62ae804870
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-07-09 17:09:19 +0100

    Use secret file or env

--- homebooru/settings.py
+++ homebooru/settings.py
@@ -22,7 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--(39sq1mfklbeb!0nc@8^h($-%-z$=qn8#teheqy$&fu=51_=*'
+
+# Read the secret key from the file 'secret.txt'
+try:
+    with open(BASE_DIR / 'secret.txt') as f:
+        SECRET_KEY = f.read().strip()
+except FileNotFoundError:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
