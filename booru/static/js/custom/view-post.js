b350650c3049d8ad63d01d9f5b82674de5ddd1db
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-07-15 17:54:36 +0100

    Add frontend lock

--- booru/static/js/custom/view-post.js
+++ booru/static/js/custom/view-post.js
@@ -15,10 +15,11 @@ class ViewPost {
      * @param {String} url url
      * @returns {Promise<Response>} response
      */
-    request(method, url = location.href) {
+    request(method, url = location.href, data = {}) {
         return fetch(url, {
             method,
-            headers: this.headers
+            headers: this.headers,
+            body: JSON.stringify(data)
         });
     }
 
@@ -49,5 +50,73 @@ class ViewPost {
             error.show('You do not have permission to delete this post.');
             return;
         }
+    }
+
+    /**
+     * Edit the post
+     * @param {Object} data post data
+     * @returns {Promise<Response>} response
+    */
+    async edit(data = {}, showError = true) {
+        // Send a PUT request to the server
+        let resp = await this.request('PUT', location.href, data);
+
+        if (showError && !resp.ok) {
+            // Show an error if it was not accepted
+            let error = new OverlayError();
+
+            let msg = await resp.text();
+
+            // TODO add this to the request function
+            if (msg.length === 0) {
+                switch (resp.status) {
+                    case 400:
+                        msg = 'Malformed request.';
+                        break;
+                    case 403:
+                        msg = 'You do not have permission to edit this post.';
+                        break;
+                    case 404:
+                        msg = 'The post was not found.';
+                        break;
+                    default:
+                        msg = 'An unknown error occurred.';
+                        break;
+                }
+            }
+
+            error.show(msg);
+        }
+
+        return resp;
+    }
+
+    /**
+     * Lock the post
+     * @returns {Promise<Response>} response
+    */
+    async lock() {
+        // Check if the user wants to lock the post
+        let confirm = new OverlayConfirm();
+
+        // Get the confirmation
+        try {
+            await confirm.show('Are you sure you want to lock this post?');
+        } catch (e) {
+            return;
+        }
+
+        // Send the request to the server
+        let resp = await this.edit({
+            locked: true
+        });
+
+        if (resp.ok) {
+            // Show a success message
+            let message = new OverlayMessage();
+            message.show('Successfully locked post.', 'Success');
+        }
+        
+        return resp;
     }
 }
