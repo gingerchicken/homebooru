ead22233b9bfa57a590a47cdb6c714ecf837dc72
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-07-30 17:39:05 +0100

    Add create post function

--- scanner/models/scanner.py
+++ scanner/models/scanner.py
@@ -57,6 +57,81 @@ class Scanner(models.Model):
     def __str__(self):
         return self.name
     
+    def create_post(self, path : str, use_default_tags = True) -> Post:
+        """Combines all of the search results and creates a post"""
+
+        # Get a md5 hash for the file
+        md5 = boorutils.get_file_checksum(path)
+
+        # Get all the results
+        results = SearchResult.objects.filter(md5=md5)
+
+        # Make sure that we got some results
+        if len(results) == 0:
+            return None
+        
+        # Find the mostly used rating
+        rating = boorutils.mode([
+            result.rating for result in results
+        ])
+
+        # Find the most common source
+        source = boorutils.mode([
+            result.source for result in results
+        ])
+
+        # Combine the tags
+        tags = {}
+
+        # Automatically add the auto tags
+        for tag in self.auto_tags.all():
+            tags[str(tag)] = tag
+
+        for result in results:
+            # Get the tags
+            raw_tags = result.tags
+
+            # Split the tags
+            tags_list = raw_tags.split(' ')
+
+            # Add the tags to the dictionary
+            for tag in tags_list:
+                # Skip the tag if we have already added it
+                if tag in tags: continue
+
+                # Skip the tag if it is invalid
+                if not Tag.is_name_valid(tag): continue
+
+                # Add the tag
+                tags[tag] = Tag.create_or_get(tag)
+        
+        # Convert the tags to a list
+        tags_list = list(tags.values())
+
+        # Make sure that we have at least one tag
+        if len(tags_list) == 0:
+            if homebooru.settings.SCANNER_USE_DEFAULT_TAGS:
+                tags_list = self.get_default_tags()
+            else:
+                return None
+
+        # Create the post
+        post = Post.create_from_file(file_path=path, owner=self.owner)
+        post.save()
+
+        # Add the tags
+        for tag in tags_list:
+            post.tags.add(tag)
+        
+        # Add other information
+        post.rating = rating
+        post.source = source
+
+        # Save changes
+        post.save()
+
+        # Return the post
+        return post
     
     def save(self, *args, **kwargs):
         # Make sure that the path exists
--- scanner/tests/models/scanner.py
+++ scanner/tests/models/scanner.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 
 from scanner.models import Booru, Scanner
+from scanner.models import Booru, Scanner, SearchResult, Tag
+from booru.models import Rating
+
 import booru.tests.testutils as booru_testutils
 import scanner.tests.testutils as scanner_testutils
+import booru.boorutils as boorutils
 
 import homebooru.settings
 
@@ -119,3 +123,180 @@ class ScannerSaveTest(TestCase):
         scanner.save()
 
         self.assertTrue(scanner.pk is not None)
+
+class ScannerCreatePostTest(TestCase):
+    fixtures = ['ratings.json']
+
+    temp_storage = booru_testutils.TempStorage()
+
+    def setUp(self):
+        # Disable default tags
+        self.og_setting = homebooru.settings.SCANNER_USE_DEFAULT_TAGS
+        homebooru.settings.SCANNER_USE_DEFAULT_TAGS = False
+
+        self.temp_storage.setUp()
+
+        # Create a scanner
+        self.scanner = Scanner(name='test_scanner', path=booru_testutils.CONTENT_PATH)
+        self.scanner.save()
+
+        # Get some booru urls
+        self.booru_urls = [i for i in scanner_testutils.VALID_BOORUS]
+
+        # Remove the / from the end of the urls and append them
+        # This probably shouldn't be a thing but it's easier to test this way
+        new_urls = []
+        for url in self.booru_urls:
+            if url[-1] == '/':
+                new_urls.append(url[:-1])
+            
+        # Add the urls
+        self.booru_urls += new_urls
+
+        # Create some boorus
+        self.boorus = []
+
+        for booru_url in self.booru_urls:
+            booru = Booru(url=booru_url, name='imagebooru ' + str(len(self.boorus)))
+            booru.save()
+
+            self.boorus.append(booru)
+        
+        # Create some results
+        self.result_path = booru_testutils.FELIX_PATH
+
+        # get the md5
+        self.md5 = boorutils.get_file_checksum(self.result_path)
+
+        self.results = []
+        for i in range(0, len(self.boorus)):
+            result = SearchResult(
+                booru=self.boorus[i],
+                md5=self.md5,
+                found=True,
+                raw_rating='safe',
+                source='https://example.com/'
+            )
+            result.save()
+
+            # Add a tag to the result
+            result.tags = 'common_tag booru_' + str(i)
+            result.save()
+
+            self.results.append(result)
+        
+        # Make sure there are more than 2 results
+        self.assertGreater(len(self.results), 2)
+    
+    def tearDown(self):
+        self.temp_storage.tearDown()
+        homebooru.settings.SCANNER_USE_DEFAULT_TAGS = self.og_setting
+
+    def test_selects_mode_rating(self):
+        """Selects the most common rating"""
+
+        # Create the post
+        post = self.scanner.create_post(self.result_path)
+        post.save()
+
+        # Make sure that the post rating is safe
+        self.assertEqual(post.rating.name, 'safe')
+
+        # Delete the post
+        post.delete()
+
+        # Update some of the results to be questionable
+        questionable = Rating.objects.get(name='questionable')
+        for i in range(1, len(self.results)):
+            self.results[i].raw_rating = str(questionable)
+            self.results[i].save()
+        
+        # Create the post
+        post = self.scanner.create_post(self.result_path)
+        post.save()
+
+        # Make sure that the post rating is questionable
+        self.assertEqual(post.rating, questionable)
+    
+    def test_selects_mode_source(self):
+        """Selects the most common source"""
+
+        # Create the post
+        post = self.scanner.create_post(self.result_path)
+        post.save()
+
+        # Make sure that the post source is correct
+        self.assertEqual(post.source, 'https://example.com/')
+
+        # Delete the post
+        post.delete()
+
+        # Update some of the results to have a different source
+        for i in range(1, len(self.results)):
+            self.results[i].source = 'https://sample.com/'
+            self.results[i].save()
+        
+        # Create the post
+        post = self.scanner.create_post(self.result_path)
+        post.save()
+
+        # Make sure that the post source is correct but different this time
+        self.assertEqual(post.source, 'https://sample.com/')
+
+    def test_merges_tags(self):
+        """Merges the tags"""
+
+        # Create the post
+        post = self.scanner.create_post(self.result_path)
+        post.save()
+
+        raw_tags = []
+
+        for tag in post.tags.all():
+            raw_tags.append(tag.tag)
+
+
+        raw_tags.sort()
+        raw_tags = ' '.join(raw_tags)
+
+        # Make sure that the post tags are correct
+        self.assertEqual(raw_tags, 'booru_0 booru_1 booru_2 booru_3 common_tag')
+    
+    def test_adds_auto_tags(self):
+        """Adds auto tags"""
+
+        # Add some auto tags to the scanner
+        test_tag = Tag(tag='test_tag')
+        test_tag.save()
+
+        self.scanner.auto_tags.add(test_tag)
+        self.scanner.save()
+
+        # Create the post
+        post = self.scanner.create_post(self.result_path)
+        post.save()
+
+        # Check the tags
+        raw_tags = []
+
+        for tag in post.tags.all():
+            raw_tags.append(tag.tag)
+
+        raw_tags.sort()
+        raw_tags = ' '.join(raw_tags)
+
+        # Make sure that the post tags are correct
+        self.assertEqual(raw_tags, 'booru_0 booru_1 booru_2 booru_3 common_tag test_tag')
+    
+    def test_none_with_no_results(self):
+        """Returns None if there are no results"""
+
+        # Delete the results
+        for result in self.results:
+            result.delete()
+
+        # Create the post
+        post = self.scanner.create_post(self.result_path)
+
+        # Make sure that the post is None
+        self.assertEqual(post, None)
