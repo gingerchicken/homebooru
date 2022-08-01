cd949d2349764ade29727ca52662c0d6796141d6
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-08-01 22:51:26 +0100

    Add scanner admin

--- scanner/admin.py
+++ scanner/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Scanner
+admin.site.register(Scanner)
+
+from .models import Booru
+admin.site.register(Booru)
+
+from .models import SearchResult
+admin.site.register(SearchResult)
