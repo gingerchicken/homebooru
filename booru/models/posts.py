fe47441f6bb49877a89b83fd3cea8943fb38fbf8
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-08-06 02:06:57 +0100

    Update sample png

--- booru/models/posts.py
+++ booru/models/posts.py
@@ -278,7 +278,7 @@ class Post(models.Model):
 
     @property
     def sample_url(self):
-        return f"samples/{self.folder}/sample_{self.md5}.jpg"
+        return f"samples/{self.folder}/sample_{self.md5}.png"
     
     @property
     def thumbnail_url(self):
@@ -331,7 +331,7 @@ class Post(models.Model):
         folder = Post.get_next_folder()
 
         # File paths
-        sample_path = settings.BOORU_STORAGE_PATH / f"samples/{folder}/sample_{md5}.jpg"
+        sample_path = settings.BOORU_STORAGE_PATH / f"samples/{folder}/sample_{md5}.png"
         thumb_path  = settings.BOORU_STORAGE_PATH / f"thumbnails/{folder}/thumbnail_{md5}.png"
         image_path  = settings.BOORU_STORAGE_PATH / f"media/{folder}/{md5}.{file_extension}"
 
