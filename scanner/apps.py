f8aa8534a30921be5c2017c652e0690809db45e2
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-09-20 23:18:20 +0000

    Patch failing test

--- scanner/apps.py
+++ scanner/apps.py
@@ -1,11 +1,16 @@
 from django.apps import AppConfig
 
+import homebooru.settings
 
 class ScannerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scanner'
 
     def ready(self):
+        # Make sure debug mode is disabled
+        if homebooru.settings.DEBUG:
+            return
+
         # Import the ScannerWatchdogs
         from .models import ScannerWatchDog
 
