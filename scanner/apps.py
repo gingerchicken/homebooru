5bd8166f7b216cb117680a2de6f79b1acea651df
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-09-21 10:27:04 +0000

    Fix test fix

--- scanner/apps.py
+++ scanner/apps.py
@@ -1,14 +1,17 @@
 from django.apps import AppConfig
 
-import homebooru.settings
+import os
 
 class ScannerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scanner'
 
     def ready(self):
-        # Make sure debug mode is disabled
-        if homebooru.settings.DEBUG:
+        # Make sure unit tests don't run this since it's not needed
+        # and if the database isn't set up yet, it will fail, where in the normal case it would be setup
+        UNIT_TEST = os.environ.get('UNIT_TEST', 'false').lower() == 'true'
+        
+        if UNIT_TEST:
             return
 
         # Import the ScannerWatchdogs
