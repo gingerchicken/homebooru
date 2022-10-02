d348d7c2a730d566c8d3608380d2dbd329116386
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-10-02 23:17:54 +0100

    Move comments admin

--- booru/admin.py
+++ booru/admin.py
@@ -12,9 +12,6 @@ admin.site.register(PostFlag)
 from .models import Rating
 admin.site.register(Rating)
 
-from .models import Comment
-admin.site.register(Comment)
-
 # Tag
 from .models import Tag
 admin.site.register(Tag)
@@ -25,3 +22,7 @@ admin.site.register(TagType)
 # Profiles
 from .models import Profile
 admin.site.register(Profile)
+
+# Comments
+from .models import Comment
+admin.site.register(Comment)
