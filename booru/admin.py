d8157698f75968d607c503f0ac4a791d91c154fe
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2023-08-31 20:03:05 +0100

    Add face preview

--- booru/admin.py
+++ booru/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 
 # Register your models here.
 
@@ -43,4 +45,19 @@ admin.site.register(TagAutomationRecord)
 from .models import FaceScan, FaceGroup, Face
 admin.site.register(FaceScan)
 admin.site.register(FaceGroup)
-admin.site.register(Face)
+
+# Create a ModelAdmin class to customize the appearance of the admin site
+@admin.register(Face)
+class FaceAdmin(admin.ModelAdmin):
+    """Custom admin page for faces."""
+
+    def face(self, obj):
+        """Returns the face as an image."""
+
+        # Get the URL for the face image using the object's primary key
+        face_image_url = reverse('face', args=[obj.id])
+
+        # Return the HTML for the image tag
+        return format_html('<img src="{}" />', face_image_url)
+            
+    list_display = ('face', )
