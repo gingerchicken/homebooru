8945abfabfd3d62882001366ac639fcce64d7486
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2023-10-28 01:22:38 +0100

    Add TODO comment

--- booru/tasks/rating_automation.py
+++ booru/tasks/rating_automation.py
@@ -9,6 +9,9 @@ from .skipper import skip_if_running
 def perform_rating_automation(post_id : int):
     """Performs rating automation on a post."""
 
+    # TODO This can create duplicate keys as it is called on save too!
+    # django.db.utils.IntegrityError: duplicate key value violates unique constraint "booru_nsfwautomationrecord_post_id_key"
+
     # Get the post
     post = Post.objects.get(id=post_id)
 
